c6ff887aa82685312b5e4bc6ad687e40d3289e4e
jeffab96 <jeffab96@users.noreply.github.com>
2020-01-15 02:09:37 -0500

    commit 1

---
+++ src/main/java/com/servicio1/rest/RestApplication.java
@@ -0,0 +1,9 @@
+package com.servicio1.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath(value = "/")
+public class RestApplication  extends Application{
+
+}
